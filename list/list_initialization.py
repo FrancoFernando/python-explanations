0eae7d712e28a9067df574a446de171bc1eafb8e
Franco Fernando <me@francofernando.com>
2022-01-20 18:20:11 +0100

    add zip

--- list/list_initialization.py
+++ list/list_initialization.py
@@ -7,3 +7,17 @@ def list_iteration():
     n = 1000
     my_int_list = [0] * n
     my_bool_list = [False] * n
+    
+    for i, num in enumerate(my_list):
+        print("index " + str(i) + " value " + str(num))
+
+    stocks = ['GOOG','AAPL','MSFT']
+    prices = [2700, 173, 310]
+
+    for stock, price in zip(stocks, prices):
+        print(f'{stock} value is {price}')
+    
+    stocks.append('XYZ')
+
+    for stock, price in itertools.zip_longest(stocks, prices, fillvalue = 0):
+        print(f'{stock} value is {price}')
