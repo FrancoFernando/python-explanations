bc9e009685ed8e84a1a77fe64af191f4baf2fd3d
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-04-20 22:57:44 +0200

    Update preserv_whitespaces.py

--- regex/preserv_whitespaces.py
+++ regex/preserv_whitespaces.py
@@ -1,8 +1,17 @@
+'''
+Sometimes the Python 🐍 split() function is not enough to process the words in string.
+
+For example, you need to use re.split() with a group to preserve whitespaces.
+'''
+
 import re
 
 def test_preserve():
-  s = " my  favourite color   is red"
-  l = re.split(r'(\s+)', s)
-  print(l)
-  capitalized = ''.join(w.capitalize() for w in l)
-  print(capitalized)
+
+  line = '4   green  12.3 \n'
+  items = re.split(r'(\s+)', line)
+
+  print(items)  
+  # ['4', '   ', 'green', '  ', '12.3', ' \n', '']
+  print(repr(''.join(items)))
+  # ' 4   green   12.3\n'
