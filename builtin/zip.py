c388b3df51d2110f52c8a33c6e0ead7d87caabe6
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-04-23 23:26:28 +0200

    Update zip.py

--- builtin/zip.py
+++ builtin/zip.py
@@ -1,7 +1,31 @@
-def test_zip():
+'''
+The Python 🐍 zip function can be used to iterate over multiple iterators in parallel.
+
+
+But be careful when supplying iterators of different length ⚠️
+
+
+Zip truncates its output silently to the shortest iterator.
+
+
+Use the zip_longest function from itertools to avoid truncation.
+'''
+
+import itertools
+
+fruits = ['apple','lemon','orange','pear']
+prices = [5, 2, 7]
+
+for fruit,price in zip(fruits, prices):
+  print(f"{fruit} price is {price}$")
+# apple price is 5$
+# lemon price is 2$
+# orange price is 7$
   
-  stocks = ['GOOG','AAPL','MFST']
-  prices = [2700, 173, 320]
+for fruit,price in itertools.zip_longest(fruits, prices):
+  print(f"{fruit} price is {price}$")
   
-  for stock,price in zip(stocks, prices)
-  print(f"{stock} value is {price}$")
+# apple price is 5$
+# lemon price is 2$
+# orange price is 7$
+# pear price is None$
