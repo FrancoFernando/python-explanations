d9c595a8ca074f360087902d67279bcca58c3234
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-05-06 11:36:01 +0200

    Update deque.py

--- collections/deque.py
+++ collections/deque.py
@@ -16,3 +16,17 @@ def test_deque():
   d.appendleft(2) # 2,3,4,5,6
   d.pop() # 2,3,4,5
   d.popleft() # 3,4,5
+  
+  
+'''
+Today I discovered an interesting use case for Python 🐍 deque.
+
+You can use it to read a file getting Unix tail behavior.
+
+All you need to do is set a maxlen equal to the number of last lines you want to read
+'''
+
+def get_last_lines(filepath, n_lines):
+    with open(filepath,'r') as file:
+        d = deque(file, maxlen=n_lines)
+    return d
